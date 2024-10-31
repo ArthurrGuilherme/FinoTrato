34c20eac6d32fe5909a2aaae592c44ffe165598b
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-10-31 17:16:29 -0300

    Js

---
+++ js/scrollrevel.js
@@ -0,0 +1,22 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('#Historia', 
+    {    
+        duration: 1000, reset: true, distance: '100px' , origin: 'left',
+    }
+);
+ScrollReveal().reveal('#Card', 
+    {    
+        duration: 1000, reset: true, distance: '100px' , origin: 'bottom',
+    }
+);
+ScrollReveal().reveal('#Card01', 
+    {    
+        duration: 1000, reset: true, distance: '100px' , origin: 'top',
+    }
+);
+ScrollReveal().reveal('#Card02', 
+    {    
+        duration: 1000, reset: true, distance: '100px' , origin: 'bottom',
+    }
+);
